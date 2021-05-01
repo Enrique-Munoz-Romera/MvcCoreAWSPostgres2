e989d10f0e84e57918e02075c2cd921694a7ede3
Enrique Muñoz <enriquemunozromera@outlook.es>
2021-05-01 19:32:47 +0200

    [ IP Private ]

--- MvcCoreAWSPostgres/Startup.cs
+++ MvcCoreAWSPostgres/Startup.cs
@@ -5,17 +5,29 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MvcCoreAWSPostgres.Data;
+using MvcCoreAWSPostgres.Repositories;
 
 namespace MvcCoreAWSPostgres
 {
     public class Startup
     {
+        IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration configuration)
+        { this.Configuration = configuration; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            String cadena =
+                Configuration.GetConnectionString("PostgresLocal");
+            services.AddTransient<RepositoryDept>();
+            services.AddDbContext<DepartamentosContext>
+                (options => options.UseNpgsql(cadena));
             services.AddControllersWithViews();
         }
 
